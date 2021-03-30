f18cdc6a7d202a031fb39b91c5560d9304b42b63
guozhaosengzs <guozhaosengzs@yahoo.com>
2021-03-29 23:57:16 -0400

    cleaning data, preping for modeling

--- script.R
+++ script.R
@@ -3,30 +3,35 @@ library(tidyverse)
 library(lme4)
 
 ###### Prepare data
-## Import all the data
+
+### Import all the data
 GLenEng <- read_csv("C:\\Users\\guozh\\Desktop\\Scopal-Ambiguity-English-and-Japanese\\data\\GestureLengthEnglish.csv")
 GLenJap <- read_csv("C:\\Users\\guozh\\Desktop\\Scopal-Ambiguity-English-and-Japanese\\data\\GestureLengthJapanese.csv")
 SLenEng <- read_csv("C:\\Users\\guozh\\Desktop\\Scopal-Ambiguity-English-and-Japanese\\data\\SentenceLengthEnglish.csv")
 SLenJap <- read_csv("C:\\Users\\guozh\\Desktop\\Scopal-Ambiguity-English-and-Japanese\\data\\SentenceLengthJapanese.csv")
 
 
-## Check column factor types, and change them accordingly
-# Length data
+### Clean column names, check each of their type and change them if needed
+
+## 2 of Length data sets
+uniformed.colnames <- c("subject", "sentence", "context", "sentence_length", "maxF0", "meanF0")
+colnames(SLenEng) <- uniformed.colnames
+colnames(SLenJap) <- uniformed.colnames
+
 sapply(SLenEng, class)
 sapply(SLenJap, class)
 
-cols <- c("subjects", "sentences", "context")
-SLenEng[cols] <- lapply(SLenEng[cols], factor)
-
-SLenEng$subjects = as.factor(SLenEng$subjects)
-
-
-
-
-
+cols  <- c("subject", "sentence", "context")
+SLenEng[cols] <- lapply(SLenEng[cols], as.factor)
+SLenJap[cols] <- lapply(SLenJap[cols], as.factor)
 
 str(SLenEng)
 str(SLenJap)
 
 
-plot(SLenEng$maxF0, SLenEng$meanF0)
+
+###### Run Models
+
+### Mixed Effect for Length data sets, using "lmer" function as the dependent variable is continuous
+
+
