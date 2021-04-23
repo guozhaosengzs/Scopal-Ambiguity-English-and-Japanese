61c75170702f5ba56f10f7643c33dab6556195cb
guozhaosengzs <guozhaosengzs@yahoo.com>
2021-04-23 00:16:24 -0400

    Finished Mix Effect Models for Gesture Length

--- script.R
+++ script.R
@@ -1,21 +1,33 @@
-## Use libraries
+## Use libraries ----
 library(tidyverse)
 library(lme4)
 library(lmerTest)
 
-###### Prepare data
-
-### Import all the data
-SLenEng <- read_csv("data\\SentenceLengthEnglish.csv")
-SLenJap <- read_csv("data\\SentenceLengthJapanese.csv")
-
+## Import all the data ----
 GLenEng <- read_csv("data\\GestureLengthEnglish.csv")
 GLenJap <- read_csv("data\\GestureLengthJapanese.csv")
 
+SLenEng <- read_csv("data\\SentenceLengthEnglish.csv")
+SLenJap <- read_csv("data\\SentenceLengthJapanese.csv")
 
-### Clean column names, check each of their type and change them if needed
+## Clean 2 of the Gesture Length data sets ----
+# Remove used columns
+GLenEng <- GLenEng[1:(length(GLenEng)-2)]
+GLenJap <- GLenJap[1:(length(GLenJap)-2)]
 
-## 2 of Length data sets
+# Correct the types of the variable
+factor_cols <- c("subject", "keyword", "context")
+GLenEng[factor_cols] <- lapply(GLenEng[factor_cols], as.factor)
+GLenJap[factor_cols] <- lapply(GLenJap[factor_cols], as.factor)
+
+numeric_cols <- c("duration", "meanF0", "maxF0")
+GLenEng[numeric_cols] <- lapply(GLenEng[numeric_cols], as.numeric)
+GLenJap[numeric_cols] <- lapply(GLenJap[numeric_cols], as.numeric)
+
+str(GLenEng)
+str(GLenJap)
+
+## Clean 2 of the Sentence Length data sets ----
 uniformed.colnames <- c("subject", "sentence", "context", "sentence_length", "maxF0", "meanF0", "keyword")
 colnames(SLenEng) <- uniformed.colnames
 colnames(SLenJap) <- uniformed.colnames
@@ -36,7 +48,55 @@ str(SLenJap)
 
 ### Mixed Effect for Length data sets, using "lmer" function as the dependent variable is continuous
 
-## English Sentence Length 
+
+## English Gesture Length ----
+
+# Gesture Duration as R.V.
+model.EngGest.Dur <- lmer(duration ~ context + 
+                              (1|keyword) + 
+                              (1|subject), 
+                          data = GLenEng)
+summary(model.EngGest.Dur)
+
+# Mean Fundamental Frequency as R.V.
+model.EngGest.meanF0 <- lmer(meanF0 ~ context + 
+                              (1|keyword) + 
+                              (1|subject), 
+                          data = GLenEng)
+summary(model.EngGest.meanF0)
+
+# Max Fundamental Frequency as R.V.
+model.EngGest.maxF0 <- lmer(maxF0 ~ context + 
+                                 (1|keyword) + 
+                                 (1|subject), 
+                             data = GLenEng)
+summary(model.EngGest.maxF0)
+
+## Japanese Gesture Length ----
+
+# Gesture Duration as R.V.
+model.JapGest.Dur <- lmer(duration ~ context + 
+                              (1|keyword) + 
+                              (1|subject), 
+                          data = GLenJap)
+summary(model.JapGest.Dur)
+
+# Mean Fundamental Frequency as R.V.
+model.JapGest.meanF0 <- lmer(meanF0 ~ context + 
+                                 (1|keyword) + 
+                                 (1|subject), 
+                             data = GLenJap)
+summary(model.JapGest.meanF0)
+
+# Max Fundamental Frequency as R.V.
+model.JapGest.maxF0 <- lmer(maxF0 ~ context + 
+                                (1|keyword) + 
+                                (1|subject), 
+                            data = GLenJap)
+summary(model.JapGest.maxF0)
+
+
+## English Sentence Length ----
 #Random Slope and Random Intercept
 
 # Sentence Length as R.V.
@@ -70,7 +130,7 @@ model.EngSent.meanF0 <- lmer(meanF0 ~ context  + (1 + context||keyword)  + (1 + 
 summary(model.EngSent.meanF0)
 
 
-## Japanese Sentence Length 
+## Japanese Sentence Length ----
 
 # Sentence_length as R.V.
 model.JapSent.Len <- lmer(sentence_length ~ context  + (1 + context||keyword)  + (1 + context||subject), 
